1e39784d69426835d9dc8a7ef065fb98304cf6c6
Oscar Aguilar <aguilop.oscar@gmail.com>
2025-04-18 22:39:15 -0600

    First commit

---
+++ src/test/java/com/grupocaos/products/athletix/AthletixApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grupocaos.products.athletix;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AthletixApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
