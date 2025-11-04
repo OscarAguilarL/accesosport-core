fe099f079ad28ee777086a2e41c033f03930403a
Oscar Aguilar <aguilop.oscar@gmail.com>
2025-11-04 10:42:12 -0600

    Add endpoint and logic for creating new events
    Introduced a REST endpoint in `EventController` to enable event creation. Defined request details, user roles, and response structure, ensuring only authorized users (organizers/admins) can access this functionality. Included a sample request configuration (`Create Event.bru`) for testing.

--- src/main/java/com/grupocaos/products/athletix/event/presentation/exception/EventController.java
+++ src/main/java/com/grupocaos/products/athletix/event/presentation/exception/EventController.java
@@ -18,6 +18,12 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.UUID;
 
+/**
+ * The {@code EventController} class exposes REST endpoints for managing events.
+ * This controller handles operations such as creating new events. It provides
+ * APIs that are secured and limited to specific roles, such as "ROLE_ORGANIZER"
+ * and "ROLE_ADMIN".
+ */
 @RestController
 @RequestMapping("/api/v1/events")
 @RequiredArgsConstructor
@@ -26,12 +32,24 @@ public class EventController {
 
     private final EventApplicationService eventApplicationService;
 
+    /**
+     * Creates a new event based on the provided request data.
+     * Only users with the "ROLE_ORGANIZER" or "ROLE_ADMIN" authority
+     * are authorized to perform this operation.
+     *
+     * @param request     the detail of the event to be created; contains fields
+     *                    such as name, description, event date, location, race type,
+     *                    distance, price, registration period, and maximum participants
+     * @param userDetails the authentication details of the currently logged-in user,
+     *                    which includes the organizer's identifier
+     * @return a ResponseEntity containing the created event's details wrapped in an EventResponse object
+     */
     @PostMapping
     @PreAuthorize("hasAnyAuthority('ROLE_ORGANIZER', 'ROLE_ADMIN')")
     public ResponseEntity<EventResponse> createEvent(
             @Valid @RequestBody CreateEventRequest request,
             @AuthenticationPrincipal CustomUserDetails userDetails
-            ) {
+    ) {
         UUID organizerId = userDetails.getUserId();
         EventResponse eventResponse = eventApplicationService.createEvent(request, organizerId);
         return ResponseEntity.status(HttpStatus.CREATED).body(eventResponse);
