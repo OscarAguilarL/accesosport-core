fe309bd6980fc02ec4324862c49ffbf946972572
Oscar Aguilar <aguilop.oscar@gmail.com>
2025-11-27 16:37:15 -0600

    Revert "fix: improved exception handling to catch all Event Exceptions"
    This reverts commit 2a660fbdaf28fd636dd765d049f368298592e27a.

--- src/main/java/com/grupocaos/products/athletix/event/domain/exception/EventNotPublishableException.java
+++ src/main/java/com/grupocaos/products/athletix/event/domain/exception/EventNotPublishableException.java
@@ -2,12 +2,7 @@ package com.grupocaos.products.athletix.event.domain.exception;
 
 public class EventNotPublishableException extends RuntimeException {
 
-    /**
-	 * Serial identifier
-	 */
-	private static final long serialVersionUID = 1540454714030567716L;
-
-	public EventNotPublishableException(String message) {
+    public EventNotPublishableException(String message) {
         super(message);
     }
 }
