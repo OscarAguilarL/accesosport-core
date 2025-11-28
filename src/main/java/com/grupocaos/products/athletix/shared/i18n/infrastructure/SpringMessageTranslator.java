611128b6e96cadd4df23be138f9b6b365a17df21
Oscar Aguilar <aguilop.oscar@gmail.com>
2025-11-28 12:55:59 -0600

    feat: add internationalization support to translate messages in the app

---
+++ src/main/java/com/grupocaos/products/athletix/shared/i18n/infrastructure/SpringMessageTranslator.java
@@ -0,0 +1,43 @@
+package com.grupocaos.products.athletix.shared.i18n.infrastructure;
+
+import java.util.Locale;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
+import org.springframework.stereotype.Component;
+
+import com.grupocaos.products.athletix.shared.i18n.domain.MessageTranslator;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+@Component
+@RequiredArgsConstructor
+@Slf4j
+public class SpringMessageTranslator implements MessageTranslator {
+
+	private final MessageSource messageSource;
+
+	@Override
+	public String translate(String key, Object... args) {
+		try {
+			Locale locale = LocaleContextHolder.getLocale();
+			return messageSource.getMessage(key, args, locale);
+		} catch (Exception e) {
+			log.warn("Translation not found for key: {} - returning key", key);
+			return key;
+		}
+	}
+
+	@Override
+	public String translate(String key, String localeString, Object... args) {
+		try {
+			Locale locale = Locale.forLanguageTag(localeString);
+			return messageSource.getMessage(key, args, locale);
+		} catch (Exception e) {
+			log.warn("Translation not found for key: {} and locale: {}", key, localeString);
+			return key;
+		}
+	}
+
+}
