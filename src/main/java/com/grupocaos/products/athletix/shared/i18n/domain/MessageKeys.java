4aa7e7fde3a545a912982de7b3ac4e02d70f1aaa
Oscar Aguilar <aguilop.oscar@gmail.com>
2025-11-28 12:55:59 -0600

    feat: add internationalization support to translate messages in the app

---
+++ src/main/java/com/grupocaos/products/athletix/shared/i18n/domain/MessageKeys.java
@@ -0,0 +1,13 @@
+package com.grupocaos.products.athletix.shared.i18n.domain;
+
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+public class MessageKeys {
+
+	@NoArgsConstructor
+	public static class AuthMessages {
+		public static String AUTHENTICATION_FAILED = "auth.errors.auth-failed";
+		public static String EMAIL_ALREADY_EXISTS = "auth.errors.email-already-exists";
+	}
+}
---
+++ src/main/java/com/grupocaos/products/athletix/shared/i18n/domain/MessageTranslator.java
@@ -0,0 +1,22 @@
+package com.grupocaos.products.athletix.shared.i18n.domain;
+
+public interface MessageTranslator {
+	/**
+	 * Translates a message using the current locale in context
+	 * 
+	 * @param key  Message key
+	 * @param args Interpolation arguments
+	 * @return Translated message
+	 */
+	String translate(String key, Object... args);
+
+	/**
+	 * Translates a message with an specific locale
+	 * 
+	 * @param key    Message key
+	 * @param locale Language key (e.g.: "es", "en")
+	 * @param args   Interpolation arguments
+	 * @return Translated message
+	 */
+	String translate(String key, String locale, Object... args);
+}
