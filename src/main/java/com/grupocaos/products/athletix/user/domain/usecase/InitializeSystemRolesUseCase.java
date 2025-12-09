38ed5902246c6004088af4a167a63d4337c8a20f
Oscar Aguilar <aguilop.oscar@gmail.com>
2025-12-08 21:25:56 -0600

    refactor: modify system initializer to follow system architecture rules

---
+++ src/main/java/com/grupocaos/products/athletix/user/domain/usecase/InitializeSystemRolesUseCase.java
@@ -0,0 +1,67 @@
+package com.grupocaos.products.athletix.user.domain.usecase;
+
+import com.grupocaos.products.athletix.shared.use_case.domain.NoInputUseCase;
+import com.grupocaos.products.athletix.user.domain.model.Role;
+import com.grupocaos.products.athletix.user.domain.model.RoleEnumeration;
+import com.grupocaos.products.athletix.user.domain.repository.RoleRepository;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * Use case to initialize User Roles
+ */
+@Slf4j
+@RequiredArgsConstructor
+public class InitializeSystemRolesUseCase extends NoInputUseCase<InitializeSystemRolesUseCase.InitializationResult> {
+
+	private final RoleRepository roleRepository;
+
+	@Override
+	protected InitializationResult doExecute(Void command) {
+
+        RoleEnumeration[] rolesToInitialize = RoleEnumeration.values();
+        int created = 0;
+        int existing = 0;
+
+        for (RoleEnumeration roleEnum: rolesToInitialize) {
+            if (roleRepository.existsByRole(roleEnum)) {
+                log.debug("Role {} already exists, skipping", roleEnum);
+                existing++;
+            } else {
+                Role role = Role.builder()
+                        .role(roleEnum)
+                        .build();
+                roleRepository.save(role);
+                log.info("Role {} created succesfully", roleEnum);
+                created++;
+            }
+        }
+
+        log.info("System roles initialization " +
+                "completed. Created {}, Existing: {}", created, existing);
+
+        return new InitializationResult(created, existing);
+	}
+
+    /**
+     * Result of the use case
+     * @param rolesCreated Number of roles created after initialization
+     * @param rolesExisting Number of roles existing before initialization
+     */
+    public record InitializationResult(Integer rolesCreated, Integer rolesExisting) {
+
+        public int getTotal() {
+            return rolesCreated + rolesExisting;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("Roles initialized - Created: %d, Already existing: %d", rolesCreated, rolesExisting);
+        }
+    }
+
+}
